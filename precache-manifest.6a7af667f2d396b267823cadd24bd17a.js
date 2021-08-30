8a707ba5094594b950283b28d644e33337c129d7
KUNAL KUMAR <87823430+callmekunal@users.noreply.github.com>
2021-08-30 22:40:11 +0530

    Updates

---
+++ precache-manifest.6a7af667f2d396b267823cadd24bd17a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d431963b3444730459cd1cbee7bdc4",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "ca1a1678a4556faafde2",
+    "url": "/todo-react/static/js/2.86be280e.chunk.js"
+  },
+  {
+    "revision": "0297a3bf3b23d1141b94ac92cf395022",
+    "url": "/todo-react/static/js/2.86be280e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac7b9ce04f3d5f1470e6",
+    "url": "/todo-react/static/js/main.7438c5b4.chunk.js"
+  },
+  {
+    "revision": "3951910ca58effa9f501",
+    "url": "/todo-react/static/js/runtime-main.552d2410.js"
+  }
+]);
